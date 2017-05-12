fbce7dd26581f5c651295ab7a7b23c27fcee6fb8
Minming Zhao <minmingzhao@gmail.com>
2017-05-11 23:56:25 -0400

    modified for project 1 term 2

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,9 @@
 #include "kalman_filter.h"
-
+#include <cmath>
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+#include <iostream>
+using namespace std;
 
 KalmanFilter::KalmanFilter() {}
 
@@ -22,6 +24,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +35,9 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	KF(y);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +45,31 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+	VectorXd h = VectorXd(3);
+	double rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+	double phi = atan2(x_(1),x_(0));
+	double rho_dot = (x_(0)*x_(2)+x_(1)*x_(3))/rho;
+	h << rho,phi,rho_dot;
+	VectorXd z_pred = h;
+	VectorXd y = z - z_pred;
+	while (y(1) >= M_PI) {
+		y(1) = y(1) - 2*M_PI;
+	}
+	while (y(1) < -M_PI) {
+			y(1) = y(1) + 2*M_PI;
+		}
+	KF(y);
+}
+
+// Universal update Kalman Filter step. Equations from the lectures
+void KalmanFilter::KF(const VectorXd &y){
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+  // New state
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
